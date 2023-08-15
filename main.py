021f5032498c76b69a1916ae26ac7ee75efcd4ee
Brian Mason <bmaso42@wgu.edu>
2023-08-15 15:55:58 -0700

    created for loop with random colored dots, complete before comparing with solution

--- main.py
+++ main.py
@@ -2,6 +2,8 @@
 ##We talk about this in the video tutorials##
 
 import colorgram
+import turtle as t
+import random
 
 # color_list = colorgram.extract('image.jpg', 100)
 # colors_rbg = []
@@ -15,9 +17,35 @@ import colorgram
 #     colors_rbg.append(rbg_tuple)
 #
 # print(colors_rbg)
+
+#CREATED COLOR LIST FROM EXTRACT, COMMENTED OUT NO LONGER NEEDED COLORGRAM CODE
 color_list = ((202, 164, 110), (149, 75, 50), (222, 201, 136), (53, 93, 123), (170, 154, 41), (138, 31, 20),
                   (134, 163, 184), (197, 92, 73), (47, 121, 86), (73, 43, 35), (145, 178, 149), (14, 98, 70),
                   (232, 176, 165), (160, 142, 158), (54, 45, 50), (101, 75, 77), (183, 205, 171), (36, 60, 74),
                   (19, 86, 89), (82, 148, 129), (147, 17, 19), (27, 68, 102), (12, 70, 64), (107, 127, 153),
                   (176, 192, 208), (168, 99, 102), (66, 64, 60), (219, 178, 183), (178, 198, 202), (112, 139, 141),
                   (254, 194, 0))
+t.colormode(255)
+tim = t.Turtle()
+tim.penup()
+# tim.setpos(-200, -200)
+# tim.color(random.choice(color_list))
+# tim.dot(20)
+# tim.setpos(-150, -200)
+# tim.dot(20)
+# print(tim.pos())
+origin = -250
+i = 0
+j = 0
+for i in range(10):
+    for j in range(10):
+        tim.setpos(origin + j*50, origin + i*50)
+        tim.color(random.choice(color_list))
+        tim.dot(20)
+
+
+
+
+sc = t.Screen()
+sc.setup(1000, 1000,0, 0)
+sc.exitonclick()
